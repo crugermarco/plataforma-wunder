8d5be7093b394486ab26c6cf30411d7b6c1a6229
crugermarco <marcocrugeruchija@gmail.com>
2025-09-10 09:54:13 -0700

    Update dashboard.js

--- login de usuarios/dashboard/dashboard.js
+++ login de usuarios/dashboard/dashboard.js
@@ -26,7 +26,7 @@ document.addEventListener('DOMContentLoaded', () => {
     empleados: './empleados/index.html',
     reportes: './reportes/index.html',
     enfermeria: './enfermeria/index.html',
-    produccion: './ZAB/index.html',
+    produccion: './zab/index.html',
     calidad: './calidad/index.html',
     configuracion: './cnc/produccion.html'
   };
@@ -97,6 +97,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 });
+
 
 
 
